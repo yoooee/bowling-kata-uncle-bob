73ee308a9b4d76cd8040e60cb7352bb76b50c7fc
Josef Staroba <jstaroba@centene.com>
2018-11-26 13:44:12 -0600

    refactor

--- src/bowling-game.spec.ts
+++ src/bowling-game.spec.ts
@@ -1,31 +1,37 @@
+class Game{
+  private _score: number = 0;
+
+  public roll(pins: number) {
+    this._score += pins;
+  }
+  public score(): number {
+    return this._score;
+  }
+};
+
+function rollMany(g: Game, n: number, pins: number) {
+    for (let i: number = 0; i < n; i++) {
+      g.roll(pins);
+    }
+}
 
 describe('Bowling Game', () => {
 
-  class Game{
-    private _score: number = 0;
-
-    public roll(pins: number) {
-      this._score += pins;
-    }
-    public score(): number {
-      return this._score;
-    }
-  };
-
-  let g: Game = new Game();
   it('should exist', () => {
+    let g: Game = new Game();
     expect(g).not.toBeUndefined();
   });
 
   describe('all gutters', () => {
 
     let g: Game = new Game();
-    for (let i: number = 0; i < 20; i++) {
-      g.roll(0);
-    }
+    let n: number = 20;
+    let pins: number = 0;
+    rollMany(g, n, pins);
+
+
 
     it('should display a score of 0', () => {
-
       expect(g.score()).toEqual(0);
     });
   });
