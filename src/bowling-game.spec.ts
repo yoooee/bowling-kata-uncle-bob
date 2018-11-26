7a8f5a12b1a89c91860206657bb27c1cd94a3d54
Josef Staroba <jstaroba@centene.com>
2018-11-26 12:38:32 -0600

    Add gutter game check

--- src/bowling-game.spec.ts
+++ src/bowling-game.spec.ts
@@ -1,10 +1,28 @@
+
 describe('Bowling Game', () => {
 
-  class Game{};
+  class Game{
+    public roll(pins: number) {}
+    public score(): number {
+      return 0;
+    }
+  };
 
   let g: Game = new Game();
   it('should exist', () => {
     expect(g).not.toBeUndefined();
   });
 
+  describe('all gutters', () => {
+
+    let g: Game = new Game();
+    for (let i = 0; i < 20; i++) {
+      g.roll(0);
+    }
+
+    it('should display a score of 0', () => {
+
+      expect(g.score()).toEqual(0);
+    });
+  });
 });
